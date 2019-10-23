01ebe815d83b1f32b9ba604e944b9ee86b857ad9
Abwao <abwaomusungu@gmail.com>
2019-10-23 10:49:37 +0300

    Minor updates

--- app.py
+++ app.py
@@ -1,40 +1,37 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
-from handy_functions import *
+from handy_functions import default_prob, get_random_sample, get_graph, descr_stats
 
 app=Flask(__name__)
 
-#get the file distribution info
-df=pd.read_csv('data/dist.csv')
+#get the distribution-info file
+df=pd.read_csv('data/distributions.csv', index_col='distribution')
 
-dist_list=list(df.distribution) # a list of the distribution names
-q=len(dist_list)
-distr='Please Select'
+distributions=list(df.index) # a list of the distribution names
+q=len(distributions)
+distr='Please Select'  #initial choice of distribution before overwriting by form-data
 
 @app.route('/')
 def index():
-	return render_template('index.html', distlist=dist_list, q=q, show_intro=True)
+	return render_template('index.html', distributions=distributions, q=q, show_intro=True)
 
-@app.route('/params',  methods=['POST', 'GET'])
-def statistics():
+@app.route('/parameters', methods=['POST', 'GET'])
+def parameters():
 	if request.method=='POST':
-		#extract form data
+		#extract form-data
 		distr=request.form['statdist']
 
 		#save selected distribution for future reference
 		with open('data/current_selection.txt', 'w') as f:
-			print(distr, file=f)
+			f.write(distr)
 
-		indx=dist_list.index(distr) #get index of distribution
-		#use index to get selected distribution's info
-		num_params=df.at[indx,'nparams']	
-		par2=df.at[indx,'param_dim'].split(',') 
-		info=df.at[indx, 'summary']
-		info=info.split('|')
+		num_params=df.at[distr,'nparams']	
+		param_def=df.at[distr,'param_def'].split(',') 
+		info=df.at[distr, 'summary'].split('|')
 
-		return render_template('index.html',d=distr ,distlist=dist_list,q=q, par=num_params, par2=par2, info=info)
+		return render_template('index.html', d=distr, distributions=distributions,q=q, num_params=num_params, param_def=param_def, info=info)
 
 @app.route('/distribution', methods=['POST', 'GET'])
 def selection():
@@ -43,33 +40,34 @@ def selection():
 		n=int(request.form['sample_size'])
 
 		with open('data/current_selection.txt', 'r') as fp:
-			d=fp.read()
-			d=str(d).strip('\n')
-		indx=df.at[dist_list.index(d),'nparams']
-		parameters=[float(request.form['par '+str(i+1)]) for i in range(indx) ]
-		nparams=len(parameters)
-		par3=df.at[dist_list.index(d),'param_dim'].split(',')
+			distr=fp.read()
+			
+		nparams=df.at[distr,'nparams']
+		parameters=[float(request.form['par '+str(i+1)]) for i in range(nparams)]
+		param_def=df.at[distr,'param_def'].split(',')
 		
 		#ensure 0<=p<=1 for affected distributions
-		if d in ['Negative Binomial','Binomial','Geometric','Bernoulli']:
+		if distr in ['Negative Binomial','Binomial','Geometric','Bernoulli']:
 			if parameters[-1]>1:
 				parameters[-1]=default_prob(parameters[-1])
 
 		#create and process the sample
-		random_sample=get_random_sample(d,n,parameters)
-		y=get_graph(random_sample)
-		z=descr_stats(random_sample)
-		title= d + ' distribution'
+		random_sample=get_random_sample(distr, n, parameters)
+		graph=get_graph(random_sample)
+		summary_stats=descr_stats(random_sample)
+		title= distr + ' distribution'
 		df2=pd.DataFrame({ title: random_sample})
+		
+		#create preview
 		if len(df2)>0:
-			pw=list(df2[title].head(20))
-			pw=[round(a,7) for a in pw ]
-			pr=enumerate(pw,start=1)
-			preview=True
+			preview=list(df2[title].round(7).head(20))
+			preview=enumerate(preview, start=1)
 		else: preview=False
+		
+		#create csv file for download
 		df2.to_csv('static/files/data.csv')
 
-		return render_template('index.html',d=d, distlist=dist_list, q=q, parameters=parameters, par3=par3, nparams=nparams, n=n, y=y,z=z,preview=preview,pr=pr)
+		return render_template('index.html',d=distr, distributions=distributions, q=q, parameters=parameters, param_def=param_def, nparams=nparams, n=n, graph=graph, summary_stats=summary_stats, preview=preview)
 
 if __name__=='__main__':
 	app.run()
