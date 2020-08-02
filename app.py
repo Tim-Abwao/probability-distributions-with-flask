544bfce0d110d39474d00e5ef402fcdc6e26a2cf
Tim-Abwao <abwaomusungu@gmail.com>
2020-08-02 17:55:03 +0300

    Refactoring code & reducing redundancies
    Former-commit-id: f97803aaaa37112edf750cd3c15238bc645a6aec

--- app.py
+++ app.py
@@ -11,41 +11,33 @@ from handy_functions import (
     clear_old_files,
     floatInt_to_int
 )
+from collections import defaultdict
 
 app = Flask(__name__)
 
 # getting distribution information
-distribution_data = pd.read_csv("data/distributions.csv",
-                                index_col="distribution")
+data = defaultdict(list)
+data['distributions'] = pd.read_csv("data/distributions.csv",
+                                    index_col="distribution")
 
 
 @app.route("/")
 def index():
-    return render_template(
-        "index.html", distributions=distribution_data, show_intro=True)
+    data['chosen_dist'] = data['graphs'] = None
+    return render_template("index.html", data=data, show_intro=True)
 
 
 @app.route("/parameters", methods=["POST", "GET"])
 def parameters():
     if request.method == "POST":
         # extracting form data
-        chosen_distr = request.form["chosen_dist"]
-        # persist current selection
-        with open("data/current_selection.txt", "w") as file:
-            file.write(chosen_distr)
-
-        dist_info = distribution_data.loc[chosen_distr]
-        input_form = "A variable to toggle the sample creation form"
-        if chosen_distr == "Please Select":
+        data['chosen_dist'] = request.form["chosen_dist"]
+        data['dist_info'] = data['distributions'].loc[data['chosen_dist']]
+        input_form = True
+        if data['chosen_dist'] == "Please Select":
             input_form = False
 
-        return render_template(
-            "index.html",
-            chosen_distr=chosen_distr,
-            dist_info=dist_info,
-            distributions=distribution_data,
-            input_form=input_form,
-        )
+        return render_template("index.html", data=data, _form=input_form)
 
     return redirect(url_for("index"))
 
@@ -53,58 +45,44 @@ def parameters():
 @app.route("/distribution", methods=["POST", "GET"])
 def selection():
     if request.method == "POST":
+        # redirect to home page if no distribution is selected
+        if not data['chosen_dist']:
+            return redirect(url_for('index'))
+
         # extracting form data
-        sample_size = int(request.form["sample_size"])
-
-        # retrieving distribution selected to know no. of parameters
-        with open("data/current_selection.txt", "r") as file:
-            chosen_distr = file.read()
-
-        dist_info = distribution_data.loc[chosen_distr]
-        # obtaining parameters
-        parameters = [
-            floatInt_to_int(float(request.form["parameter " + str(i + 1)]))
-            for i in range(dist_info["no_of_parameters"])
-        ]
+        data['sample_size'] = int(request.form["sample_size"])
+        data['parameters'] = [floatInt_to_int(
+            float(request.form["parameter " + str(i + 1)]))
+                for i in range(data['dist_info']["no_of_parameters"])
+            ]
         # ensuring 0<=p<=1 for affected distributions
-        if chosen_distr in {"Negative Binomial", "Binomial",
-                            "Geometric", "Bernoulli"}:
+        if data['chosen_dist'] in {"Negative Binomial", "Binomial",
+                                   "Geometric", "Bernoulli"}:
             # probability(p) is the rightmost parameter
-            parameters[-1] = validate_probability(parameters[-1])
+            data['parameters'][-1] = \
+                validate_probability(data['parameters'][-1])
 
         # creating and processing the sample
-        random_sample = get_random_sample(chosen_distr, sample_size,
-                                          *parameters)
-        graphs = get_graphs(random_sample)
-        summary_stats = descriptive_stats(random_sample)
+        random_sample = get_random_sample(
+            data['chosen_dist'], data['sample_size'], *data['parameters'])
+        data['graphs'] = get_graphs(random_sample)
+        data['summary_stats'] = descriptive_stats(random_sample)
         sample_data = pd.Series(random_sample,
-                                name=chosen_distr + " distribution")
+                                name=data['chosen_dist'] + " distribution")
 
         # generating a preview
         if len(sample_data) > 0:
-            preview = enumerate(sample_data.head(20).round(7).to_numpy())
+            preview = sample_data.head(20).round(7)
         else:
-            preview = False
+            preview = []
 
-        # clear old samples
-        clear_old_files("csv")
-
-        # creating a csv file of the sample, for download
-        sample_name = chosen_distr + "_sample_data.csv"
+        # exporting the sample to csv file available for download
+        clear_old_files("csv")  # removes previously saved samples
+        sample_name = data['chosen_dist'] + "_sample_data.csv"
         sample_data.to_csv("static/files/" + sample_name, index=False)
-
-        return render_template(
-            "index.html",
-            chosen_distr=chosen_distr,
-            dist_info=dist_info,
-            distributions=distribution_data,
-            sample_size=sample_size,
-            preview=preview,
-            summary_stats=summary_stats,
-            graphs=graphs,
-            sample_name=sample_name,
-            parameters=parameters,
-        )
+        return render_template("index.html", data=data, preview=preview,
+                               sample_name=sample_name
+                               )
     return redirect(url_for("index"))
 
 
